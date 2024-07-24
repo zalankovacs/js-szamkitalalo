f5fd51b264438d4a632260f0604272bd0b13ec4d
Tamas Kovacs <tamas.kovacs@bearingpoint.com>
2024-07-25 01:55:04 +0300

    form validation

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let guessListContainer = document.querySelector('.js-guess-container');
     let currentGuessField = document.querySelector('.js-guess-field'); 
     let newGameButton = document.querySelector('.js-new-game-btn');
+    let guessSubmitButton = document.querySelector('.js-submit-guess')
 
     let target;
 
@@ -21,7 +22,40 @@ document.addEventListener("DOMContentLoaded", () => {
     function startGame() {
         resetGame();
         target = generateTarget();
-        console.log("Generated target number: " + target);  // Debugging: ellenőrizd a konzolban
+    }
+
+    function submitGuess(event)
+    {
+        event.preventDefault();
+        let guessValue = Number.parseInt(currentGuessField.value);
+        console.log(guessValue, typeof guessValue);
+        if(validateGuess())
+        {
+            alert('klikk');
+        }
+    }
+
+    function addGuessValidation()
+    {
+        currentGuessField.classList.add('is-invalid');
+    }
+
+    function removeGuessValidation()
+    {
+        currentGuessField.classList.remove('is-invalid');
+    }
+
+    function validateGuess()
+    {
+        let guessValue = Number.parseInt(currentGuessField.value);
+        currentGuessField.value = guessValue;
+        if(Number.isNaN(guessValue) || guessValue < 1 || guessValue > 100)
+        {
+            addGuessValidation();
+            return false; 
+        }
+            removeGuessValidation();
+            return true;
     }
 
     startGame();
@@ -29,4 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();  // Megakadályozzuk a form submit eseményt
         startGame();
     });
+    guessSubmitButton.addEventListener('click', submitGuess);
+    currentGuessField.addEventListener('focus', removeGuessValidation);
+    currentGuessField.addEventListener('change', validateGuess);
 });
