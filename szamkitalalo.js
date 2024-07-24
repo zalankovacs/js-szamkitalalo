4e5aaf0f7a80bdb3134bc0b43c86e08f1072b3f2
Tamas Kovacs <tamas.kovacs@bearingpoint.com>
2024-07-24 16:42:57 +0300

    markup + target generalasa

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,14 +1,6 @@
-/*
+function generateTarget()
+{
+    return Math.floor(Math.random() * 100) + 1;
+}
 
-A szamitogep gondol 1 szamra 1 es 100 kozott. A felhasznalo egy input mezobe irja a tipjpjet. A szamitogep minden tippre a kovetkezok egyiket valaszolja:
-- a keresett szam kisebb
-- a keresett szam nagyobb
--gratulalok eltalaltad
-
-Amint eltalaltad a keresett szamot a jatek veget er. A jatek vegen kiirjuk a tippek szamat.
-Barmikor lehet uj jatokot inditani. Ilyenkor a korabbi tippek torlodnek es a szamitogep uj szamot general.
-
-A jatek soran a tippek listaja idorendi sorrendben megjelenik. Felul talalhato a legutolso tipped,
-alul pedig az elso tipped.
-
-*/
+console.log(generateTarget());
