1de544ede96b96e268e8ab7d0607405132efd5fe
Tamas Kovacs <tamas.kovacs@bearingpoint.com>
2024-07-25 14:14:36 +0300

    tippek kiertekelese

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let guessListContainer = document.querySelector('.js-guess-container');
     let currentGuessField = document.querySelector('.js-guess-field'); 
     let newGameButton = document.querySelector('.js-new-game-btn');
-    let guessSubmitButton = document.querySelector('.js-submit-guess')
+    let guessSubmitButton = document.querySelector('.js-submit-guess');
+    let guessListDisplay = document.querySelector('.js-guess-list');
 
     let target;
+    let guessList = [];
+
+    const guessResults = {
+        KISEBB: 'kisebb',
+        NAGYOBB: 'nagyobb',
+        HELYES: 'talalt',
+    };
 
     function generateTarget() {
         return Math.floor(Math.random() * 100) + 1;
@@ -12,11 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function resetGame() {
         // korábbi találgatások törlése, de az űrlap megtartása
-        let guessList = document.querySelectorAll('.js-guess');
-        guessList.forEach(guess => guess.remove());
+        guessList = [];
+        guessListDisplay.innerHTML = '';
         
         // input mező tartalmának törlése
         currentGuessField.value = ''; 
+        removeGuessValidation();
     }
 
     function startGame() {
@@ -24,38 +33,72 @@ document.addEventListener("DOMContentLoaded", () => {
         target = generateTarget();
     }
 
-    function submitGuess(event)
-    {
+    function addGuess(guessValue, result) {
+        guessList.unshift({ guessValue, result });
+    }
+
+    function compareGuessToTarget(guess) {
+        if (guess < target) {
+            addGuess(guess, guessResults.NAGYOBB);
+        } else if (guess > target) {
+            addGuess(guess, guessResults.KISEBB);
+        } else {
+            addGuess(guess, guessResults.HELYES);
+        }
+        renderGuessList();
+    }
+
+    function submitGuess(event) {
         event.preventDefault();
         let guessValue = Number.parseInt(currentGuessField.value);
-        console.log(guessValue, typeof guessValue);
-        if(validateGuess())
-        {
-            alert('klikk');
+        if (validateGuess()) {
+            compareGuessToTarget(guessValue);
         }
     }
 
-    function addGuessValidation()
-    {
+    function addGuessValidation() {
         currentGuessField.classList.add('is-invalid');
     }
 
-    function removeGuessValidation()
-    {
+    function removeGuessValidation() {
         currentGuessField.classList.remove('is-invalid');
     }
 
-    function validateGuess()
-    {
+    function validateGuess() {
         let guessValue = Number.parseInt(currentGuessField.value);
-        currentGuessField.value = guessValue;
-        if(Number.isNaN(guessValue) || guessValue < 1 || guessValue > 100)
-        {
+        if (Number.isNaN(guessValue) || guessValue < 1 || guessValue > 100) {
             addGuessValidation();
             return false; 
         }
-            removeGuessValidation();
-            return true;
+        removeGuessValidation();
+        return true;
+    }
+
+    function renderGuessList() {
+        let html = '';
+        let guessType, guessText;
+
+        for (let { guessValue, result } of guessList) {
+            if (result === guessResults.KISEBB) {
+                guessType = 'warning';
+                guessText = 'a keresett szám kisebb';
+            } else if (result === guessResults.NAGYOBB) {
+                guessType = 'primary';
+                guessText = 'a keresett szám nagyobb';
+            } else {
+                guessType = 'success';
+                guessText = 'gratulálok eltaláltad!';
+            }
+            html += `
+                <div class="row js-guess">
+                    <div class="col col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 alert alert-${guessType}" role="alert">
+                        ${guessValue} - ${guessText}
+                    </div>
+                </div>
+            `;
+        }
+
+        guessListDisplay.innerHTML = html;
     }
 
     startGame();
@@ -64,6 +107,5 @@ document.addEventListener("DOMContentLoaded", () => {
         startGame();
     });
     guessSubmitButton.addEventListener('click', submitGuess);
-    currentGuessField.addEventListener('focus', removeGuessValidation);
-    currentGuessField.addEventListener('change', validateGuess);
+    currentGuessField.addEventListener('input', validateGuess);
 });
